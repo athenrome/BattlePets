f15148374495056d7bc0815ab344a6c8d9c1ff71
athenrome <athenrome@gmail.com>
2015-04-26 23:06:44 +1000

    Project creation/upgrade to unity 5.1
    Copied the files from a unity 5.0 to a 5.1

---
+++ Battle Pets/Assets/Scripts/DataController.cs
@@ -0,0 +1,193 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DataController : MonoBehaviour {
+
+    GameController game = new GameController();
+
+    public TextAsset PetDataText;
+    public TextAsset AbilityDataText;
+
+    List<Ability> AbilityList = new List<Ability>();
+    List<Pet> PetList = new List<Pet>();
+    
+    
+
+
+	void Awake () {
+        LoadPetData();
+        LoadAbilityData();
+        SendData(AbilityList, PetList);
+	}
+
+    void SendData(List<Ability> AbilityList, List<Pet> PetList)
+    {
+        game.AbilitiesList = AbilityList;
+        //print(game.AbilitiesList.Count);
+        game.AvailablePets = PetList;
+    }
+
+    void LoadAbilityData()
+    {
+
+        List<AbilityData> RawAbilityData;
+
+        System.IO.StringReader reader = new System.IO.StringReader(AbilityDataText.text);
+        CsvHelper.CsvParser parser = new CsvHelper.CsvParser(reader);
+        CsvHelper.CsvReader CsvReader = new CsvHelper.CsvReader(parser);
+        CsvReader.Configuration.AutoMap<AbilityData>();
+
+        RawAbilityData = CsvReader.GetRecords<AbilityData>().ToList();
+
+        ProcessAbilityData(RawAbilityData);
+
+    }
+
+    void ProcessAbilityData(List<AbilityData> RawAbilityData)
+    {
+
+        Ability NewAbility;
+        List<Ability> NewAbilityList = new List<Ability>();
+
+        foreach(AbilityData RawAbility in RawAbilityData)
+        {
+            string Name = RawAbility.Name;
+            ElementType ElementType = ChooseElement(RawAbility.ElementType);
+            AttackType AttackType = ChooseAttackType(RawAbility.AttackType);
+            int BaseDamage = ConvertToInt(RawAbility.BaseDamage);
+            float RechargeTime = ConvertToFloat(RawAbility.RechargeTime);
+
+            NewAbility = new Ability(Name, ElementType, AttackType, BaseDamage, RechargeTime);
+
+            NewAbilityList.Add(NewAbility);
+            
+        }
+
+        AbilityList = NewAbilityList;
+
+        print("Loaded " + AbilityList.Count + "Abilities");
+        
+
+        
+        
+    }
+
+    void LoadPetData()
+    {
+
+        List<PetData> RawPetData;
+
+        System.IO.StringReader reader = new System.IO.StringReader(PetDataText.text);
+        CsvHelper.CsvParser parser = new CsvHelper.CsvParser(reader);
+        CsvHelper.CsvReader CsvReader = new CsvHelper.CsvReader(parser);
+        CsvReader.Configuration.AutoMap<PetData>();
+
+        RawPetData = CsvReader.GetRecords<PetData>().ToList();
+
+        ProcessPetData(RawPetData);
+
+    }
+
+    void ProcessPetData(List<PetData> RawPetData)
+    {
+        Pet NewPet;
+
+        List<Pet> NewPetList = new List<Pet>();
+
+        foreach(PetData RawPet in RawPetData)
+        {
+            string PetName = RawPet.Name;
+            ElementType ElementType = ChooseElement(RawPet.ElementType);
+            int PetHealth = ConvertToInt(RawPet.Health);
+            int PetStrength = ConvertToInt(RawPet.Strength);
+            float PetSpeed = ConvertToFloat(RawPet.Speed);
+            int CurrentXP = ConvertToInt(RawPet.Xp);
+            int CurrentLvl = ConvertToInt(RawPet.Lvl);
+            float FireRes = ConvertToFloat(RawPet.FireRes);
+            float EarthRes = ConvertToFloat(RawPet.EarthRes);
+            float WaterRes = ConvertToFloat(RawPet.WaterRes);
+            float AirRes = ConvertToFloat(RawPet.AirRes);
+
+
+            NewPet = new Pet(PetName, ElementType, PetHealth, PetStrength, PetSpeed, CurrentXP, CurrentLvl, FireRes, EarthRes, WaterRes, AirRes);
+
+            NewPetList.Add(NewPet);
+            
+            
+        }
+
+        PetList = NewPetList;
+        print("Loaded " + PetList.Count + "Pets");
+
+    }
+
+    AttackType ChooseAttackType(string ToConvert)
+    {
+
+        AttackType NewPetAttack = AttackType.Default;
+
+        switch (ToConvert)
+        {
+            case "Beam":
+                NewPetAttack = AttackType.Beam;
+                break;
+            case "Heal":
+                NewPetAttack = AttackType.Heal;
+                break;
+            case "Projectile":
+                NewPetAttack = AttackType.Projectile;
+                break;
+            case "Melee":
+                NewPetAttack = AttackType.Melee;
+                break;
+        }
+
+        return NewPetAttack;
+
+    }
+
+    ElementType ChooseElement(string ToConvert)
+    {
+
+        ElementType NewPetElement = ElementType.Default;
+
+        switch (ToConvert)
+        {
+            case "Air":
+                NewPetElement = ElementType.Air;
+                break;
+            case "Earth":
+                NewPetElement = ElementType.Earth;
+                break;
+            case "Fire":
+                NewPetElement = ElementType.Fire;
+                break;
+            case "Water":
+                NewPetElement = ElementType.Water;
+                break;
+        }
+
+        return NewPetElement;
+    }
+
+    int ConvertToInt(string ToConvert)
+    {
+        int NewInt;
+
+        NewInt = int.Parse(ToConvert);
+
+        return NewInt;
+    }
+
+
+    float ConvertToFloat(string ToConvert)
+    {
+        float NewFloat;
+
+        NewFloat = float.Parse(ToConvert);
+
+        return NewFloat;
+    }
+}
---
+++ Battle Pets/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+    public Player PlayerCharacter;
+
+    DataController data = new DataController();
+
+    public List<Pet> AvailablePets = new List<Pet>();
+    public List<Ability> AbilitiesList = new List<Ability>();
+
+	// Use this for initialization
+	void Start () {
+        print(AvailablePets.Count);
+
+        PlayerCharacter = CreatePlayer();
+	}
+	
+	// Update is called once per frame
+	void Update () {       	
+	}
+
+    Player CreatePlayer()
+    {
+        print("asdf" + AvailablePets.Count);
+
+        Player NewPlayer;
+
+        NewPlayer = new Player("Player", GetPets());
+
+        return NewPlayer;
+    }
+
+    List<Pet>GetPets()
+    {
+        int maxPets = 4;
+        
+        List<Pet> PlayerPets = new List<Pet>();
+
+        for (int i = 0; PlayerPets.Count < maxPets; i++)
+        {
+            int Chooser = Random.Range(0, AvailablePets.Count);
+
+            PlayerPets.Add(AvailablePets[i]);
+            AvailablePets.Remove(AvailablePets[i]);
+        }
+
+        return PlayerPets;
+    }
+}
